bf929940e180f45f16f8a1a436c617f22af199d0
Marlon Rogério <msrogerio9@gmail.com>
2022-05-26 19:09:59 -0500

    captura de seguidores

--- app/cwarler/crawler.py
+++ app/cwarler/crawler.py
@@ -2,6 +2,7 @@ from config import ENDC, WARNING
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from ..models import Folowers
 from .. import db
 
@@ -89,33 +90,57 @@ def openFollowers(driver):
 
 
 def getFollowers(driver):
-        if openFollowers(driver) == False:
-            return False
-        followers, _ = getFollowingFollwers(driver)
-        if not followers:
-            return False
-        cont = 1
-        div = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/div/div/div/div[2]/ul')))
-        while True:
-            if cont == int(followers):
+    if openFollowers(driver) == False:
+        return False
+    followers, _ = getFollowingFollwers(driver)
+    if not followers:
+        return False
+    li = []
+    while True:
+        try:
+            driver.implicitly_wait(10)
+            driver.execute_script(
+                """
+                var element = document.getElementsByClassName("isgrP")[0];
+                element.scrollTop = element.scrollHeight
+                """
+            )
+        except Exception as ex:
+            print(f'{WARNING}[INFO] Chegou ao fim da barra {ex}.{ENDC}')
+            break
+        
+        try: 
+            li = driver.find_elements_by_class_name('T0kll')
+            print('[INFO] Tamanho do vetor', len(li))
+            if len(li) >= int(followers):
                 break
-            while True:
-                try:
-                    username = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, f'/html/body/div[6]/div/div/div/div[2]/ul/div/li[{cont}]/div/div[2]/div[1]/div/div/span/a/span'))).text
-                    name = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, f'/html/body/div[6]/div/div/div/div[2]/ul/div/li[{cont}]/div/div[1]/div[2]/div[2]'))).text
-                    print(f'[INFO] Nome de usuário: {username}, usuário {name}.')
-                    fl = Folowers()
-                    fl.username = username
-                    fl.name = name
-                    db.session.add(fl)
-                    db.session.commit()
-                    cont +=1 
-                except Exception as ex:
-                    print(f'[INFO] Não encontrou mais elementos nesta unidade carregada. {ex}')
-                    break
-            try:
-                driver.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', div)
-                driver.implicitly_wait(5)
-            except Exception as ex:
-                print(f'{WARNING}[INFO] Chegou ao fim da barra.{ENDC}')
-                break
+            else:
+                continue
+        except Exception as ex:
+            pass
+
+    for i in li:
+        print(i.text)
+
+            # _7UhW9 xLCgt qyrsm KV-D4 se6yk T0kll
+            # _7UhW9 xLCgt qyrsm KV-D4 se6yk T0kll
+
+
+# while True:
+#                 try:
+#                     username = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, f'/html/body/div[6]/div/div/div/div[2]/ul/div/li[{cont}]/div/div[2]/div[1]/div/div/span/a/span'))).text
+#                     cont += 1
+#                 except Exception as ex:
+#                     cont += 1 
+#                     print(f'[INFO] Não capturou o "username" | Contador: {cont}| {ex}')
+#                     break
+
+#                 # print(f'[INFO] Nome de usuário: {username}, usuário {name}.')
+#                 print(f'[INFO] Nome de usuário: {username}.')
+#                 fl = Folowers()
+#                 fl.username = username
+#                 db.session.add(fl)
+#                 db.session.commit()
+#                 cont +=1 
+
+            
