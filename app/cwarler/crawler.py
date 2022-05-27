f1fb44e9473166345d7610f4889b4bc15acfd184
Marlon Rogério <msrogerio9@gmail.com>
2022-05-26 23:52:15 -0500

    mostagem de seguidores e seguindo

--- app/cwarler/crawler.py
+++ app/cwarler/crawler.py
@@ -1,9 +1,10 @@
+import time
 from config import ENDC, WARNING
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from ..models import Folowers
+from ..models import Following, Folowers
 from .. import db
 
 
@@ -79,6 +80,16 @@ def getFollowingFollwers(driver):
         return None, None
 
 
+def getMyUserName(driver):
+    try:
+        username = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/div[1]/h2'))).text
+        print(f'[INFO] Username: {username}')
+        return username
+    except Exception as ex:
+        print(f'{WARNING}[ERRO] Não conseguiu abrir o perfil do usuário.{ex}{ENDC}')
+        return None
+
+
 def openFollowers(driver):
     try:
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/section/main/div/header/section/ul/li[2]/a/div/span'))).click()
@@ -89,13 +100,79 @@ def openFollowers(driver):
         return False
 
 
-def getFollowers(driver):
+def closeModalFollowers(driver):
+    try:
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[6]/div/div/div/div[1]/div/div[3]/div/button'))).click()
+        print(f'[INFO] Fechou o modal de Folowers')
+        return True
+    except Exception as ex:
+        print(f'{WARNING}[ERRO] Não conseguiu fechar o modal de Folowers{ex}{ENDC}')
+        return False
+
+
+def openFollowing(driver):
+    try:
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/section/main/div/header/section/ul/li[3]/a/div/span'))).click()
+        print(f'[INFO] Abriu o modal de Following')
+        return True
+    except Exception as ex:
+        print(f'{WARNING}[ERRO] Não conseguiu abrir o modal de Following{ex}{ENDC}')
+        return False
+
+
+def getFollowers(driver, my_id):
     if openFollowers(driver) == False:
-        return False
-    followers, _ = getFollowingFollwers(driver)
+        return None, None
+    followers, following = getFollowingFollwers(driver)
     if not followers:
-        return False
-    li = []
+        return None, None
+    li_followers = []
+    while True:
+        try:
+            driver.implicitly_wait(10)
+            driver.execute_script(
+                """
+                var element = document.getElementsByClassName("isgrP")[0];
+                element.scrollTop = element.scrollHeight
+                """
+            )
+        except Exception as ex:
+            print(f'{WARNING}[INFO] Chegou ao fim da barra {ex}.{ENDC}')
+        
+        try: 
+            li_followers = driver.find_elements_by_class_name('T0kll')
+            print('[INFO] Tamanho do vetor', len(li_followers))
+            if len(li_followers) >= int(followers):
+                break
+            else:
+                continue
+        except Exception as ex:
+            pass
+
+    for i in li_followers:
+        if ' ' in i.text or '\n' in i.text:
+            continue
+        follower = Folowers()
+        follower.username = i.text
+        follower.user_id = my_id
+        db.session.add(follower)
+        db.session.commit()
+        print('[INFO] Registro adicionado no banco.')
+
+
+    #### > > > < < < ####
+    # driver.refresh()
+    closeModalFollowers(driver)
+    time.sleep(5)
+
+    if openFollowing(driver) == False:
+        return None, None
+    time.sleep(5)
+    
+    if not following:
+        return None, None
+
+    li_following = []
     while True:
         try:
             driver.implicitly_wait(10)
@@ -110,37 +187,23 @@ def getFollowers(driver):
             break
         
         try: 
-            li = driver.find_elements_by_class_name('T0kll')
-            print('[INFO] Tamanho do vetor', len(li))
-            if len(li) >= int(followers):
+            li_following = driver.find_elements_by_class_name('T0kll')
+            print('[INFO] Tamanho do vetor', len(li_following))
+            if len(li_following) >= int(following):
                 break
             else:
                 continue
         except Exception as ex:
             pass
 
-    for i in li:
-        print(i.text)
+    for i in li_following:
+        if ' ' in i.text or '\n' in i.text or 'Seguindo' in i.text:
+            continue
+        following = Following()
+        following.username = i.text
+        following.user_id = my_id
+        db.session.add(following)
+        db.session.commit()
+        print('[INFO] Registro adicionado no banco.')
 
-            # _7UhW9 xLCgt qyrsm KV-D4 se6yk T0kll
-            # _7UhW9 xLCgt qyrsm KV-D4 se6yk T0kll
-
-
-# while True:
-#                 try:
-#                     username = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, f'/html/body/div[6]/div/div/div/div[2]/ul/div/li[{cont}]/div/div[2]/div[1]/div/div/span/a/span'))).text
-#                     cont += 1
-#                 except Exception as ex:
-#                     cont += 1 
-#                     print(f'[INFO] Não capturou o "username" | Contador: {cont}| {ex}')
-#                     break
-
-#                 # print(f'[INFO] Nome de usuário: {username}, usuário {name}.')
-#                 print(f'[INFO] Nome de usuário: {username}.')
-#                 fl = Folowers()
-#                 fl.username = username
-#                 db.session.add(fl)
-#                 db.session.commit()
-#                 cont +=1 
-
-            
+    return li_followers, li_followers
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from crypt import methods
+import email
 from . import main
-from flask import render_template, request
+from .. models import Users
+from flask import redirect, render_template, request, url_for
 from ..cwarler.controller import driver as new_driver
 from ..cwarler.crawler import *
 
@@ -47,7 +48,32 @@ def login():
             driver.quit()
             return render_template('login.html')
 
-        getFollowers(driver)
-        
-        driver.quit()
-        return render_template('home.html')
+        username = getMyUserName(driver)
+        if not username:
+            return render_template('login.html')
+        temp = Users.query.filter_by(username=username).first()
+        if temp:
+            id= temp.id
+        else:
+            user = Users()
+            user.username = username
+            user.email = usuario
+            db.session.add(user)
+            db.session.commit()
+            id = Users.query.filter_by(username=username).first().id
+        li_followers, li_following = getFollowers(driver, id)
+        if not li_following and not li_followers:
+            return render_template('login.html') 
+        # driver.quit()
+        return redirect(url_for('.data_returned', username=username))
+
+
+@main.route('/data-returned')
+def data_returned():
+    username=request.args.get('username')
+    print(username)
+    id = 1
+    # id = Users.query.filter_by(username=username).first()
+    followers = Folowers.query.filter(Folowers.user_id==id).all()
+    following = Following.query.filter(Following.user_id==id).all()
+    return render_template('home.html', followers=followers, following=following)
